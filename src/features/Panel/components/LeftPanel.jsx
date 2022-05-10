c5e13fcd7bb4cb679a30436bc8986c81a7010ba7
SevenNorth <qibeidu@163.com>
2022-05-10 22:45:34 +0800

    移除未使用的变量

--- src/features/Panel/components/LeftPanel.jsx
+++ src/features/Panel/components/LeftPanel.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 import { connect } from 'react-redux';
-import { Tabs } from 'antd';
+// import { Tabs } from 'antd';
 
 import './index.less';
 import { useWidgets } from '../../../utils'
